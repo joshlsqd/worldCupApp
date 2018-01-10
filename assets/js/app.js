350dd8d9de8ea1a69731ec5831fd76fc02a6796e
Joshua Cook <jcook@lynxsolutions.com>
2018-01-09 19:06:44 -0800

    adding clock

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,4 +38,36 @@ function displayFixtures() {
 
 }
 
+function countdown() {
+    var now = new Date();
+    var eventDate = new Date(2018, 5, 14, -2,  );
+
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var s = Math.floor(remTime/ 1000);
+    var m = Math.floor(s/ 60);
+    var h = Math.floor(m/ 60);
+    var d = Math.floor(h/ 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+    setTimeout(countdown, 1000)
+
+}
+
+countdown();
 displayFixtures();
